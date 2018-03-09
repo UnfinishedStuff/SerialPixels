ec003a5e1bb93df683e5b66078b0b757895be5bb
JC <20929510+Shoe-Pi@users.noreply.github.com>
2018-03-09 23:50:46 +0000

    Uploaded SerialPixelsPC
    Initial commit.

---
+++ SerialPixelsPC/SerialPixelsPC/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace SerialPixelsPC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 58);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(253, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(253, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Get Serial Ports";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(12, 85);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(253, 39);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Choose a colour";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(81, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "R,G,B values:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(276, 186);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SerialPixelsPC/SerialPixelsPC/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace SerialPixelsPC
+{
+    public partial class Form1 : Form
+    {
+
+        static SerialPort serialPort;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        //Button to get COM ports and add them to comboBox1
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            //Clear all current items in the combobox list
+            comboBox1.Items.Clear();
+
+            //Get an array of ports and assign it to ports
+            string[] ports = SerialPort.GetPortNames();
+
+            //Go through each port in ports and add it to the Items in comboBox1
+            foreach (string port in ports)
+            {
+                comboBox1.Items.Add(port);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //When the button is pressed, check the colour selected by the colorDialog which pops up
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //Set bytes R, G and B equal to the selected colour's Red, Green and Blue values
+                byte R = colorDialog1.Color.R;
+                byte G = colorDialog1.Color.G;
+                byte B = colorDialog1.Color.B;
+
+                label1.Text = String.Format("R,G,B = {0}, {1}, {2}", R, G, B);
+                
+                // Create a new SerialPort object with default settings
+                serialPort = new SerialPort(Convert.ToString(comboBox1.Text));
+
+                // Set the baudrate (the default of 9600 is a tad slow)
+                serialPort.BaudRate = 115200;
+
+                //Open the port
+                serialPort.Open();
+                //Send "<R,G,B>".  Data are only used by the Trinket if enclosed between square brackets. 
+                serialPort.WriteLine(Convert.ToString(R) + "," + Convert.ToString(G) + "," + Convert.ToString(B));
+                //Close the port
+                serialPort.Close();
+            }
+        }
+
+        
+    }
+}
